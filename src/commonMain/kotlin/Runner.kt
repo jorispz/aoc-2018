36d790b3eba9cc21b2acace1a77b5aad3745419b
Joris Portegies Zwart <joris.portegies.zwart@ximedes.com>
2018-12-16 18:57:11 +0100

    Day 16

--- src/commonMain/kotlin/Runner.kt
+++ src/commonMain/kotlin/Runner.kt
@@ -17,7 +17,8 @@ object Puzzles {
         Puzzle(12) to p12,
         Puzzle(13) to p13,
         Puzzle(14) to p14,
-        Puzzle(15) to p15
+        Puzzle(15) to p15,
+        Puzzle(16) to p16
     )
 
     fun run(day: Int, part: Int? = null) {
